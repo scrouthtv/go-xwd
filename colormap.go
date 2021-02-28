78aae05995d955236131c2a436645aceb00c6ba1
Lenni <lenivh24@gmail.com>
2021-02-28 19:54:49 +0100

    XWDColor implements image/color.Color

--- colormap.go
+++ colormap.go
@@ -2,6 +2,8 @@ package xwd
 
 import (
 	"io"
+	"log"
+	"image/color"
 	"errors"
 	"encoding/binary"
 )
@@ -18,7 +20,20 @@ type XWDColor struct {
 
 const colorSize = 4+2+2+2+1+1
 
+var opaqAlpha uint32 = uint32(color.Opaque.A) | uint32(color.Opaque.A) << 16
+
+// RGBA implements the image/color.Color.RGBA() method
+func (c *XWDColor) RGBA() (r, g, b, a uint32) {
+	return uint32(c.Red) << 16, uint32(c.Green) << 16, uint32(c.Blue) << 16, 0xffffffff
+}
+
 type XWDColorMap []XWDColor
+
+const colormapKeySize = 1 // uint8, this is the size in the pixmap
+
+func (c *XWDColorMap) Get(i int) XWDColor {
+	return (*c)[i]
+}
 
 // ReadColorMap reads the colormap from an xwd image, provided that the header has already been read
 // and discarded from the reader.
@@ -42,13 +57,14 @@ func ReadColorMap(r io.Reader, h *XWDFileHeader) (XWDColorMap, error) {
 			return nil, errors.New("partial color read")
 		}
 		m[i] = XWDColor{
-			binary.BigEndian.Uint32(buf[0:4]) << 8, // I don't know why we're missing 2 bytes in the output, but I don't care
+			binary.BigEndian.Uint32(buf[0:4]),// << 8 seems to be wrong
 			binary.BigEndian.Uint16(buf[4:6]),
 			binary.BigEndian.Uint16(buf[6:8]),
 			binary.BigEndian.Uint16(buf[8:10]),
 			uint8(buf[10]),
 			uint8(buf[11]),
 		}
+		log.Printf("Read %sas %s", hecateHex(buf), m[i].String())
 	}
 
 	return m, nil
